3c3da7a07f5fc295dd53f55d052e806b97724690
huangkevin88 <55661601+huangkevin88@users.noreply.github.com>
2020-01-04 21:51:09 +0800

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+"   Time now: "+t.Format(time.UnixDate))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+
+												      "   Time now: "+t.Format(time.UnixDate))).Do(); err != nil {
 					log.Print(err)
 				}
 			}
