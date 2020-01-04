9bd117fa0fd500c6716e27b735b7c2f2c3795f6f
huangkevin88 <55661601+huangkevin88@users.noreply.github.com>
2020-01-04 23:14:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -64,11 +64,18 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if err3 != nil {
 					fmt.Println(err3)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+
-												      "\nUTC time: "+now.In(local1).Format(time.UnixDate)+
-												      "\nTaipei time: "+now.In(local2).Format(time.UnixDate)+
-												      "\nUSA time: "+now.In(local3).Format(time.UnixDate))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(
+					if(message.Text == "time" || "Time" || "TIME"){
+					event.ReplyToken, linebot.NewTextMessage(message.Text+
+									      "\nUTC time: "+now.In(local1).Format(time.UnixDate)+
+									      "\nTaipei time: "+now.In(local2).Format(time.UnixDate)+
+									      "\nUSA time: "+now.In(local3).Format(time.UnixDate))).Do(); err != nil {
 					log.Print(err)
+					}
+					else{
+					event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+					}
 				}
 			}
 		}
